7df007de4412aee286434995773e30b204c6af05
mcfulmer13 <macfulmer@live.com>
2021-09-02 00:21:44 -0400

    error fixes

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-const { fstat } = require('fs');
-const inquirer = require('inquirer');
-const html = require('./html.js');
 const fs = require('fs');
+const inquirer = require('inquirer');
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const genOutput = require('./src/html');
+const orgStructure = [];
 
 
-  return inquirer.prompt([
+init = () => {
+ function teamGen() { inquirer.prompt([
+    { type: 'list', name: 'role', message: 'choose worker role', choices: ["Engineer","Manager", "Intern", "Exit"]},
+  ]).then((answers) => {
+    switch (answers.role) {
+      case "Manager": genManager(answers); break;
+      case "Intern": genIntern(); break;
+      case "Engineer": genEngineer(); break;
+      case "Exit": genMembers(); 
+      default: 
+      genMembers();
+    } 
+  }); 
+  }
+
+  const genManager = () => { inquirer.prompt([
     {
       type: 'input',
       message: 'Enter Manager Name:',
@@ -18,97 +36,83 @@ const fs = require('fs');
       {
         type: 'input',
         message: 'Enter Manager Email:',
-        name: 'Email',
+        name: 'email',
       },
       {
       type: 'input',
-        message: 'Enter Member Office Number:',
+        message: 'Enter manager Office Number:',
         name: 'office',
-      },
-      {
-        type: 'list',
-        message: 'Add a Memeber:',
-        name: 'positions',
-        choices: ["Intern", "Engineer", "NONE!!!"]
-      }
-  ])
-  .then((answers) => {
-    // Use user feedback for position!!
-    if(answers.positions == "Intern"){
-      return inquirer.prompt([
-        {
-          type: 'input',
-          message: 'Enter Intern Name:',
-          name: 'iname',
-        },
-        {
-          type: 'input',
-          message: 'Enter Intern ID:',
-          name: 'iid',
-        },
-        {
-          type: 'input',
-          message: 'Enter Intern Email:',
-          name: 'iemail',
-        },
-        {
-          type: 'input',
-          message: 'Enter Intern School:',
-          name: 'ischool',
-        },
-        {
-          type: 'list',
-          message: 'Add a Memeber:',
-          name: 'positions',
-          choices: ["Intern", "Engineer", "NONE!!!"]
-        }
-      ])   
-    }
-  }) 
-  .then((answers) => {
-    // Use user feedback for position!!
-    if(answers.positions == "Engineer"){
-      return inquirer.prompt([
-        {
-          type: 'input',
-          message: 'Enter Engineer Name:',
-          name: 'ename',
-        },
-        {
-          type: 'input',
-          message: 'Enter Engineer ID:',
-          name: 'eid',
-        },
-        {
-          type: 'input',
-          message: 'Enter Engineer Email:',
-          name: 'eemail',
-        },
-        {
-          type: 'input',
-          message: 'Enter Engineer Github:',
-          name: 'egithub',
-        },
-        {
-          type: 'list',
-          message: 'Add a Memeber:',
-          name: 'positions',
-          choices: ["Intern", "Engineer", "NONE!!!"]
-        }
-      ])   
-    } 
-  })
-  .then((answers) => {
-    console.log(answers.name)
-    console.log(answers.email)
-    console.log(answers.id)
-    fs.writeFile("index.html", html(data), (err) => {
-      if (err)
-        console.log(err);
-      else {
-        console.log("File written successfully\n");
-      }
-    });
-  })
+      }      
+  ]).then(answers => {const manager = new Manager(answers.name, answers.id, answers.email, answers.office);
+    orgStructure.push(manager) 
+    console.log(answers.name, answers.id, answers.email, answers.office)
+    teamGen();
+  });
+}
 
+  const genEngineer = () => { inquirer.prompt([
+    {
+      type: 'input',
+      message: 'Enter Engineer Name:',
+      name: 'name',
+    },
+    {
+      type: 'input',
+      message: 'Enter Engineer ID:',
+      name: 'id',
+    },
+    {
+      type: 'input',
+      message: 'Enter Engineer Email:',
+      name: 'email',
+    },
+    {
+      type: 'input',
+      message: 'Enter Engineer Github:',
+      name: 'github',
+    }
+  ]).then(answers => {const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+    orgStructure.push(engineer) 
+    console.log(answers.name, answers.id, answers.email, answers.github)
+    teamGen();
+  });
+}
+  
+  const genIntern = () => { inquirer.prompt([
+    {
+      type: 'input',
+      message: 'Enter Intern Name:',
+      name: 'name',
+    },
+    {
+      type: 'input',
+      message: 'Enter Intern ID:',
+      name: 'id',
+    },
+    {
+      type: 'input',
+      message: 'Enter Intern Email:',
+      name: 'email',
+    },
+    {
+      type: 'input',
+      message: 'Enter Intern School:',
+      name: 'school',
+    }
+  ]).then(answers => {const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+    orgStructure.push(intern) 
+    console.log(answers.name, answers.id, answers.email, answers.school)
+    teamGen();
+  });
+  }
+  
+  function genMembers() {let HTMLContent = genOutput(orgStructure)
+    const filePath = "./dist/index.html";
+    fs.writeFileSync(filePath, HTMLContent, "utf-8")
+  }
+  
+  teamGen();
+  
+}
+init();
 
